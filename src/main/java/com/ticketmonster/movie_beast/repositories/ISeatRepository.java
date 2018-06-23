9da1b0349fc82acc203a35f301bf8e9f61206216
Markos Korvesis <korvesism@gmail.com>
2018-06-24 02:09:56 +0300

    Authenticated User Can Book Tickets {show_id: String, seat : String}, minor fixes.

--- src/main/java/com/ticketmonster/movie_beast/repositories/ISeatRepository.java
+++ src/main/java/com/ticketmonster/movie_beast/repositories/ISeatRepository.java
@@ -18,7 +18,7 @@ public interface ISeatRepository extends JpaRepository<Seat, Integer> {
     List<Seat> findAllByShowId(@Param("show_id") Integer show_id);
 
     @Query("SELECT s FROM Seat s WHERE s.booking_id = :booking_id")
-    List<Seat> findByBookingId(@Param("booking_id") Integer booking_id);
+    Seat findByBookingId(@Param("booking_id") Integer booking_id);
 
     @Query("SELECT s FROM Seat s WHERE s.seat_reserved = true AND s.paid = false")
     List<Seat> findAllReservedButNonPaid();
--- src/main/java/com/ticketmonster/movie_beast/rest_controllers/BookingCreationController.java
+++ src/main/java/com/ticketmonster/movie_beast/rest_controllers/BookingCreationController.java
@@ -1,21 +1,34 @@
 package com.ticketmonster.movie_beast.rest_controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ticketmonster.movie_beast.custom_exceptions.CustomException;
 import com.ticketmonster.movie_beast.models.Booking;
+import com.ticketmonster.movie_beast.models.Seat;
 import com.ticketmonster.movie_beast.models.Show;
 import com.ticketmonster.movie_beast.models.User;
 import com.ticketmonster.movie_beast.repositories.IBookingRepository;
+import com.ticketmonster.movie_beast.repositories.ISeatRepository;
 import com.ticketmonster.movie_beast.repositories.IShowRepository;
 import com.ticketmonster.movie_beast.repositories.IUserRepository;
+import org.apache.pdfbox.cos.COSInputStream;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.PDStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.Consumes;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RestController
@@ -30,54 +43,79 @@ public class BookingCreationController {
     @Autowired
     private IShowRepository showRepository;
 
-    @PostMapping("/bookTicket")
-    public ResponseEntity<?> bookTicket()/*(@RequestBody Integer event_id, @RequestBody String seat, @RequestBody Integer numberOfTickets) */ {
+    @Autowired
+    private ISeatRepository seatRepository;
 
-        int show_id = 2;
-        int numberOfTickets = 2;
-        int seat = 1;
+    @PostMapping("/bookTicket")
+    @Consumes("application/json")
+    public ResponseEntity<?> bookTicket(@RequestBody ObjectNode bookingObjNode) {
+
+        Integer show_id = bookingObjNode.get("show_id").asInt();
+        Integer seat = bookingObjNode.get("seat").asInt();
+        Integer seatIndex = 0;
+        boolean isSeatAvailable = false;
+
         Show show = showRepository.getOne(show_id);
 
-        if (numberOfTickets <= show.getAvailable_seats()) {
-            //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            //User user = userRepository.findByEmail(authentication.getPrincipal().toString());
-            User user = userRepository.findByEmail("admin@dummy.com");
+        List<Seat> showSeats = seatRepository.findAllByShowId(show.getShow_id());
+
+        for (int i = 0; i < showSeats.size(); i++) {
+            System.out.println(showSeats.get(i).getSeat_id().toString());
+            if (showSeats.get(seat).getBooking_id() == null) {
+                isSeatAvailable = true;
+                seatIndex = showSeats.get(seat).getSeat_id();
+            }
+        }
+
+        if (show.getAvailable_seats() >= 1 && isSeatAvailable) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = userRepository.findByEmail(authentication.getName());
 
             List<Booking> bookings = new ArrayList<Booking>();
 
-            for (int i = 0; i < numberOfTickets; i++) {
-                Booking booking = new Booking();
-                booking.setShow_id(show.getShow_id());
-                booking.setUser_id(user.getUser_id());
-                booking.setBooking_cost(show.getShow_cost());
-                booking.setSeat_id(seat);
-                bookings.add(bookingRepository.save(booking));
-            }
-            show.setAvailable_seats(show.getAvailable_seats() - numberOfTickets);
+            Booking booking = new Booking();
+            booking.setShow_id(show.getShow_id());
+            booking.setUser_id(user.getUser_id());
+            booking.setBooking_cost(show.getShow_cost());
+            booking.setSeat_id(seatIndex);
+            booking.setBooking_date(show.getShow_date());
+            bookings.add(bookingRepository.save(booking));
+
+            Optional<Seat> optionalBookedSeat = seatRepository.findById(seatIndex);
+            Seat bookedSeat = optionalBookedSeat.get();
+            bookedSeat.setSeat_reserved(true);
+            bookedSeat.setBooking_id(bookings.get(0).getBooking_id());
+            show.setAvailable_seats(show.getAvailable_seats() - 1);
+
             showRepository.save(show);
+            seatRepository.save(bookedSeat);
 
             return new ResponseEntity<List<Booking>>(bookings, HttpStatus.OK);
+        } else if (!isSeatAvailable) {
+            return new ResponseEntity<>(
+                    new CustomException("Sorry but it seems that the seat you requested got suddenly unavailable."), HttpStatus.NOT_ACCEPTABLE);
         } else {
             return new ResponseEntity<>(
-                    new CustomException("Sorry but it seems we don't have that many tickets available."), HttpStatus.RESET_CONTENT);
+                    new CustomException("Sorry but it seems we don't have any seats left."), HttpStatus.NOT_ACCEPTABLE);
         }
     }
 
-    //    @GetMapping(path = "/{event_id}/printTicket.pdf", produces = "application/pdf") // FIXME:
-    //    public ResponseEntity<?> printTicket() {
-    //        PDDocument pdf = new PDDocument();
-    //
-    //        pdf.addPage(new PDPage());
-    //        PDStream pdStream = new PDStream(pdf);
-    //
-    //        try {
-    //            COSInputStream cosInputStream = pdStream.createInputStream();
-    //
-    //            return ResponseEntity.ok().contentLength(pdStream.getLength())
-    //                    .contentType(MediaType.parseMediaType("application/octet-stream"))
-    //                    .body(new InputStreamResource(cosInputStream));
-    //        } catch (IOException e) {
-    //            return ResponseEntity.badRequest().build();
-    //        }
-    //    }
+    // TODO: figure out how to print reserved ticket
+    @GetMapping(path = "/shows/{show_id}/printedTicket.pdf", produces = "application/pdf")
+    public ResponseEntity<?> printTicket(@PathVariable Integer show_id) {
+        PDDocument pdf = new PDDocument();
+
+        pdf.addPage(new PDPage());
+        PDStream pdStream = new PDStream(pdf);
+
+        try {
+            COSInputStream cosInputStream = pdStream.createInputStream();
+
+            return ResponseEntity.ok().contentLength(pdStream.getLength())
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(new InputStreamResource(cosInputStream));
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
