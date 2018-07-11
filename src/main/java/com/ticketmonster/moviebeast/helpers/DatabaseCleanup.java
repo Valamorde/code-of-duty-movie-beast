ac6b126d76a9bc92f5c1ca1ff75cfdeeae805bfd
Markos Korvesis <korvesism@gmail.com>
2018-07-11 20:46:15 +0300

    Added Logger to Database Cleanup.

--- src/main/java/com/ticketmonster/moviebeast/helpers/DatabaseCleanup.java
+++ src/main/java/com/ticketmonster/moviebeast/helpers/DatabaseCleanup.java
@@ -2,10 +2,14 @@ package com.ticketmonster.moviebeast.helpers;
 
 import com.ticketmonster.moviebeast.models.SeatReservation;
 import com.ticketmonster.moviebeast.repositories.ISeatReservationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,17 +18,28 @@ import java.util.List;
 @Component
 public class DatabaseCleanup {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ISeatReservationRepository seatReservationRepository;
 
     @Scheduled(fixedRate = 180000)
+    @Transactional
     public void cleanupUnpaidReservations(){
-        List<SeatReservation> seatReservations = seatReservationRepository.findAllBySeatReservedIsTrueAndSeatPaidIsFalse();
+        try {
+            List<SeatReservation> seatReservations = seatReservationRepository.findAllBySeatReservedIsTrueAndSeatPaidIsFalse();
 
-        for(int i=0; i< seatReservations.size(); i++){
-            seatReservations.get(i).setSeatReserved(false);
-            seatReservations.get(i).setUser(null);
-            seatReservationRepository.save(seatReservations.get(i));
+            logger.warn("Initiating Database Cleanup... " + new Date());
+
+            for (int i = 0; i < seatReservations.size(); i++) {
+                seatReservations.get(i).setSeatReserved(false);
+                seatReservations.get(i).setUser(null);
+                seatReservationRepository.save(seatReservations.get(i));
+            }
+
+            logger.warn("Database Cleanup Complete... " + new Date());
+        }catch (Exception e){
+            logger.error("Database Cleanup failed... ", e);
         }
     }
 }
