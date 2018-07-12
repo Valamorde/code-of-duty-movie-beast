96956db1a76e9ce4f16158aea6504a95e41e8e06
Markos Korvesis <korvesism@gmail.com>
2018-07-13 00:20:27 +0300

    <NPM_I> error messages

--- src/app/login-register/login/login.component.ts
+++ src/app/login-register/login/login.component.ts
@@ -2,25 +2,24 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Auth/auth.service';
 import { Router } from '@angular/router'
-
-
-
+import { HttpErrorResponse } from '@angular/common/http';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-
-
-
-
 })
 export class LoginComponent implements OnInit {
 
-  loginUserData = {}
+  loginUserData = {};
+  errorMessages = [];
 
-  constructor(private _auth: AuthService,
-    private _router: Router) { }
+  constructor(
+    private _auth: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -28,15 +27,30 @@ export class LoginComponent implements OnInit {
   loginUser() {
     this._auth.loginUser(this.loginUserData)
       .subscribe(
-        res => {
+        (res) => {
           localStorage.setItem('token', res.token)
           this._router.navigate(['/movies'])
         },
-        err => console.log(err)
+        (err) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              this.showError();
+            }
+          }
+        }
       )
   }
+
   navigate() {
     this._router.navigate(['/signup'])
+  }
+
+  showError() {
+    this.errorMessages.push({ severity: 'error', summary: 'Bad Credentials', detail: 'Woops! Wrong username/password. Check for typos and give it another try!' });
+  }
+
+  clearError() {
+    this.errorMessages = [];
   }
 
 }
--- src/app/login-register/signup/signup.component.ts
+++ src/app/login-register/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Auth/auth.service';
 import { Router } from '@angular/router'
+import { HttpErrorResponse } from '@angular/common/http';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-register',
@@ -8,26 +11,40 @@ import { Router } from '@angular/router'
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  errorMessages = [];
   registerUserData = {}
   constructor(private _auth: AuthService,
-              private _router: Router) { }
+    private _router: Router) { }
 
   ngOnInit() {
   }
 
   registerUser() {
     this._auth.registerUser(this.registerUserData)
-    .subscribe(
-      res => {
-        this._router.navigate(['/login'])
-      },
-      err => console.log(err)
-    )      
+      .subscribe(
+        (res) => {
+          this._router.navigate(['/login'])
+        },
+        (err) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              this.showError();
+            }
+          }
+        }
+      )
   }
 
-  navigate (){
+  navigate() {
     this._router.navigate(['/login'])
+  }
+
+  showError() {
+    this.errorMessages.push({ severity: 'error', summary: 'Email already exists.', detail: "Hmm, that email exists, try using another or login if it's yours." });
+  }
+
+  clearError() {
+    this.errorMessages = [];
   }
 
 }
